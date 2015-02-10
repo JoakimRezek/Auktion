2882648814136e1b7a2014f999d07a9b4e3a7fe5
JoakimRezek <joakim.rezek@gmail.com>
2015-02-10 12:12:09 +0100

    läggtillny klasser

---
+++ Auktion_GUI/src/com/jbay/view/KundView.java
@@ -0,0 +1,270 @@
+package com.jbay.view;
+
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JLabel;
+
+import java.awt.GridBagConstraints;
+
+import javax.swing.JTextField;
+
+import java.awt.Insets;
+
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.SwingConstants;
+
+
+public class KundView extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField personNummerField;
+	private JTextField förNamnField;
+	private JTextField efterNamnField;
+	private JLabel lblNewLabel_1;
+	private JTextField adressField;
+	private JLabel lblNewLabel_2;
+	private JTextField postNummerField;
+	private JLabel lblOrt;
+	private JTextField ortField;
+	private JLabel lblNewLabel_3;
+	private JTextField telefonNummerField;
+	private JLabel lblEmail;
+	private JTextField emailField;
+	private JButton btnRegister;
+	private JButton btnAvbryt;
+	
+	private static KundView frame;
+	
+	private JDBC_Connection controller = new JDBC_Connection();
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					KundView frame = new KundView();
+					frame.setVisible(true);
+					frame.pack();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public static KundView getsingleton(){
+		if (frame == null) 
+			frame = new KundView();
+		
+		
+		return frame;
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public KundView() {
+		setTitle("Registrera ny Kund");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblFrnamn = new JLabel("Personnummer:");
+		GridBagConstraints gbc_lblFrnamn = new GridBagConstraints();
+		gbc_lblFrnamn.insets = new Insets(10, 10, 10, 10);
+		gbc_lblFrnamn.gridx = 0;
+		gbc_lblFrnamn.gridy = 0;
+		contentPane.add(lblFrnamn, gbc_lblFrnamn);
+		
+		personNummerField = new JTextField();
+		GridBagConstraints gbc_personNummerField = new GridBagConstraints();
+		gbc_personNummerField.gridwidth = 2;
+		gbc_personNummerField.insets = new Insets(10, 10, 10, 10);
+		gbc_personNummerField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_personNummerField.gridx = 2;
+		gbc_personNummerField.gridy = 0;
+		contentPane.add(personNummerField, gbc_personNummerField);
+		personNummerField.setColumns(10);
+		
+		JLabel lblEfternamn = new JLabel("Förnamn:");
+		GridBagConstraints gbc_lblEfternamn = new GridBagConstraints();
+		gbc_lblEfternamn.insets = new Insets(10, 10, 10, 10);
+		gbc_lblEfternamn.gridx = 0;
+		gbc_lblEfternamn.gridy = 1;
+		contentPane.add(lblEfternamn, gbc_lblEfternamn);
+		
+		förNamnField = new JTextField();
+		GridBagConstraints gbc_förNamnField = new GridBagConstraints();
+		gbc_förNamnField.gridwidth = 2;
+		gbc_förNamnField.insets = new Insets(10, 10, 10, 10);
+		gbc_förNamnField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_förNamnField.gridx = 2;
+		gbc_förNamnField.gridy = 1;
+		contentPane.add(förNamnField, gbc_förNamnField);
+		förNamnField.setColumns(10);
+		
+		JLabel lblEfterNamn = new JLabel("Efternamn:");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(10, 10, 10, 10);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 2;
+		
+		contentPane.add(lblEfterNamn, gbc_lblNewLabel);
+		
+		efterNamnField = new JTextField();
+		GridBagConstraints gbc_efterNamnField = new GridBagConstraints();
+		gbc_efterNamnField.gridwidth = 2;
+		gbc_efterNamnField.insets = new Insets(10, 10, 10, 10);
+		gbc_efterNamnField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_efterNamnField.gridx = 2;
+		gbc_efterNamnField.gridy = 2;
+		contentPane.add(efterNamnField, gbc_efterNamnField);
+		efterNamnField.setColumns(10);
+		
+		lblNewLabel_1 = new JLabel("Adress:");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(10, 10, 10, 10);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 3;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		adressField = new JTextField();
+		GridBagConstraints gbc_adressField = new GridBagConstraints();
+		gbc_adressField.gridwidth = 2;
+		gbc_adressField.insets = new Insets(10, 10, 10, 10);
+		gbc_adressField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_adressField.gridx = 2;
+		gbc_adressField.gridy = 3;
+		contentPane.add(adressField, gbc_adressField);
+		adressField.setColumns(10);
+		
+		lblNewLabel_2 = new JLabel("Postnummer:");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(10, 10, 10, 10);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 4;
+		contentPane.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		postNummerField = new JTextField();
+		GridBagConstraints gbc_postNummerField = new GridBagConstraints();
+		gbc_postNummerField.gridwidth = 2;
+		gbc_postNummerField.insets = new Insets(10, 10, 10, 10);
+		gbc_postNummerField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_postNummerField.gridx = 2;
+		gbc_postNummerField.gridy = 4;
+		contentPane.add(postNummerField, gbc_postNummerField);
+		postNummerField.setColumns(10);
+		
+		lblOrt = new JLabel("Ort:");
+		GridBagConstraints gbc_lblOrt = new GridBagConstraints();
+		gbc_lblOrt.insets = new Insets(10, 10, 10, 10);
+		gbc_lblOrt.gridx = 0;
+		gbc_lblOrt.gridy = 5;
+		contentPane.add(lblOrt, gbc_lblOrt);
+		
+		ortField = new JTextField();
+		GridBagConstraints gbc_ortField = new GridBagConstraints();
+		gbc_ortField.gridwidth = 2;
+		gbc_ortField.insets = new Insets(10, 10, 10, 10);
+		gbc_ortField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_ortField.gridx = 2;
+		gbc_ortField.gridy = 5;
+		contentPane.add(ortField, gbc_ortField);
+		ortField.setColumns(10);
+		
+		lblNewLabel_3 = new JLabel("Telefonnummer:");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(10, 10, 10, 10);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 6;
+		contentPane.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		telefonNummerField = new JTextField();
+		GridBagConstraints gbc_telefonNummerField = new GridBagConstraints();
+		gbc_telefonNummerField.gridwidth = 2;
+		gbc_telefonNummerField.anchor = GridBagConstraints.NORTH;
+		gbc_telefonNummerField.insets = new Insets(10, 10, 10, 10);
+		gbc_telefonNummerField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_telefonNummerField.gridx = 2;
+		gbc_telefonNummerField.gridy = 6;
+		contentPane.add(telefonNummerField, gbc_telefonNummerField);
+		telefonNummerField.setColumns(10);
+		
+		lblEmail = new JLabel("E-mail:");
+		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
+		gbc_lblEmail.insets = new Insets(10, 10, 10, 10);
+		gbc_lblEmail.gridx = 0;
+		gbc_lblEmail.gridy = 7;
+		contentPane.add(lblEmail, gbc_lblEmail);
+		
+		emailField = new JTextField();
+		GridBagConstraints gbc_emailField = new GridBagConstraints();
+		gbc_emailField.gridwidth = 2;
+		gbc_emailField.insets = new Insets(10, 10, 10, 10);
+		gbc_emailField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_emailField.gridx = 2;
+		gbc_emailField.gridy = 7;
+		contentPane.add(emailField, gbc_emailField);
+		emailField.setColumns(10);
+		
+		btnRegister = new JButton("Registera");
+		btnRegister.setHorizontalAlignment(SwingConstants.LEFT);
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String personNummer = personNummerField.getText();
+				String förNamn = förNamnField.getText();
+				String efterNamn = efterNamnField.getText();
+				String adress = adressField.getText();
+				String postNummer = postNummerField.getText();
+				String ort = ortField.getText();
+				String telefonNummer = telefonNummerField.getText();
+				String email = emailField.getText();
+
+				controller.läggTillNyLeverantör(orgNummer, företag, provision, kntPerson, email, telefon);
+			}
+		});
+		GridBagConstraints gbc_btnRegister = new GridBagConstraints();
+		gbc_btnRegister.insets = new Insets(10, 10, 10, 10);
+		gbc_btnRegister.anchor = GridBagConstraints.EAST;
+		gbc_btnRegister.gridx = 2;
+		gbc_btnRegister.gridy = 8;
+		contentPane.add(btnRegister, gbc_btnRegister);
+		
+		btnAvbryt = new JButton("Avbryt");
+		btnAvbryt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		GridBagConstraints gbc_btnAvbryt = new GridBagConstraints();
+		gbc_btnAvbryt.gridx = 3;
+		gbc_btnAvbryt.gridy = 8;
+		contentPane.add(btnAvbryt, gbc_btnAvbryt);
+	}
+
+}	
+
+
---
+++ Auktion_GUI/src/com/jbay/view/LeverantörView.java
@@ -0,0 +1,221 @@
+package com.jbay.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JLabel;
+
+import java.awt.GridBagConstraints;
+
+import javax.swing.JTextField;
+
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+
+
+public class LeverantörView extends JFrame {
+
+	private JPanel contentPane;
+	
+	
+	private static LeverantörView frame;
+	private JTextField orgNrField;
+	private JTextField företagField;
+	private JTextField provisionField;
+	private JTextField kntPersonField;
+	private JTextField emailField;
+	private JTextField telefonField;
+	
+	
+	private JDBC_Connection controller = new JDBC_Connection();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LeverantörView frame = new LeverantörView();
+					frame.setVisible(true);
+					frame.pack();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public static LeverantörView getsingleton(){
+		if (frame == null) 
+			frame = new LeverantörView();
+		
+		
+		return frame;
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LeverantörView() {
+		setTitle("Registrera ny Leverantör");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblOrgNr = new JLabel("Organisationsnummer:");
+		GridBagConstraints gbc_lblOrgNr = new GridBagConstraints();
+		gbc_lblOrgNr.insets = new Insets(10, 10, 10, 10);
+		gbc_lblOrgNr.gridx = 0;
+		gbc_lblOrgNr.gridy = 0;
+		contentPane.add(lblOrgNr, gbc_lblOrgNr);
+		
+		orgNrField = new JTextField();
+		GridBagConstraints gbc_orgNrField = new GridBagConstraints();
+		gbc_orgNrField.gridwidth = 2;
+		gbc_orgNrField.insets = new Insets(10, 10, 10, 10);
+		gbc_orgNrField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_orgNrField.gridx = 1;
+		gbc_orgNrField.gridy = 0;
+		contentPane.add(orgNrField, gbc_orgNrField);
+		orgNrField.setColumns(10);
+		
+		JLabel lblFöretag = new JLabel("Företagsnamn:");
+		GridBagConstraints gbc_lblFöretag = new GridBagConstraints();
+		gbc_lblFöretag.insets = new Insets(10, 10, 10, 10);
+		gbc_lblFöretag.gridx = 0;
+		gbc_lblFöretag.gridy = 1;
+		contentPane.add(lblFöretag, gbc_lblFöretag);
+		
+		företagField = new JTextField();
+		GridBagConstraints gbc_företagField = new GridBagConstraints();
+		gbc_företagField.gridwidth = 2;
+		gbc_företagField.insets = new Insets(10, 10, 10, 10);
+		gbc_företagField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_företagField.gridx = 1;
+		gbc_företagField.gridy = 1;
+		contentPane.add(företagField, gbc_företagField);
+		företagField.setColumns(10);
+		
+		JLabel lblProvision = new JLabel("Provision:");
+		GridBagConstraints gbc_lblProvision = new GridBagConstraints();
+		gbc_lblProvision.insets = new Insets(10, 10, 10, 10);
+		gbc_lblProvision.gridx = 0;
+		gbc_lblProvision.gridy = 2;
+		contentPane.add(lblProvision, gbc_lblProvision);
+		
+		provisionField = new JTextField();
+		GridBagConstraints gbc_provisionField = new GridBagConstraints();
+		gbc_provisionField.gridwidth = 2;
+		gbc_provisionField.insets = new Insets(10, 10, 10, 10);
+		gbc_provisionField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_provisionField.gridx = 1;
+		gbc_provisionField.gridy = 2;
+		contentPane.add(provisionField, gbc_provisionField);
+		provisionField.setColumns(10);
+		
+		JLabel lblKontaktPerson = new JLabel("Kontaktperson:");
+		GridBagConstraints gbc_lblKontaktPerson = new GridBagConstraints();
+		gbc_lblKontaktPerson.insets = new Insets(10, 10, 10, 10);
+		gbc_lblKontaktPerson.gridx = 0;
+		gbc_lblKontaktPerson.gridy = 3;
+		contentPane.add(lblKontaktPerson, gbc_lblKontaktPerson);
+		
+		kntPersonField = new JTextField();
+		kntPersonField.setText("");
+		GridBagConstraints gbc_kntPersonField = new GridBagConstraints();
+		gbc_kntPersonField.gridwidth = 2;
+		gbc_kntPersonField.insets = new Insets(10, 10, 10, 10);
+		gbc_kntPersonField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_kntPersonField.gridx = 1;
+		gbc_kntPersonField.gridy = 3;
+		contentPane.add(kntPersonField, gbc_kntPersonField);
+		kntPersonField.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("E-mail:");
+		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
+		gbc_lblEmail.insets = new Insets(10, 10, 10, 10);
+		gbc_lblEmail.gridx = 0;
+		gbc_lblEmail.gridy = 4;
+		contentPane.add(lblEmail, gbc_lblEmail);
+		
+		emailField = new JTextField();
+		GridBagConstraints gbc_emailField = new GridBagConstraints();
+		gbc_emailField.gridwidth = 2;
+		gbc_emailField.insets = new Insets(10, 10, 10, 10);
+		gbc_emailField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_emailField.gridx = 1;
+		gbc_emailField.gridy = 4;
+		contentPane.add(emailField, gbc_emailField);
+		emailField.setColumns(10);
+		
+		JLabel lblTlfnr = new JLabel("Telefonnummer:");
+		GridBagConstraints gbc_lblTlfnr = new GridBagConstraints();
+		gbc_lblTlfnr.insets = new Insets(10, 10, 10, 10);
+		gbc_lblTlfnr.gridx = 0;
+		gbc_lblTlfnr.gridy = 5;
+		contentPane.add(lblTlfnr, gbc_lblTlfnr);
+		
+		telefonField = new JTextField();
+		GridBagConstraints gbc_telefonField = new GridBagConstraints();
+		gbc_telefonField.gridwidth = 2;
+		gbc_telefonField.insets = new Insets(10, 10, 10, 10);
+		gbc_telefonField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_telefonField.gridx = 1;
+		gbc_telefonField.gridy = 5;
+		contentPane.add(telefonField, gbc_telefonField);
+		telefonField.setColumns(10);
+		
+		JButton btnRegistrera = new JButton("Registrera");
+		btnRegistrera.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String orgNummer = orgNrField.getText();
+				String företag = företagField.getText();
+				int provision = Integer.parseInt(provisionField.getText());
+				String kntPerson = kntPersonField.getText();
+				String email = emailField.getText();
+				String telefon = telefonField.getText();
+
+				controller.läggTillNyLeverantör(orgNummer, företag, provision, kntPerson, email, telefon);
+				
+			}
+		});
+		GridBagConstraints gbc_btnRegistrera = new GridBagConstraints();
+		gbc_btnRegistrera.insets = new Insets(10, 10, 10, 10);
+		gbc_btnRegistrera.gridx = 1;
+		gbc_btnRegistrera.gridy = 7;
+		contentPane.add(btnRegistrera, gbc_btnRegistrera);
+		
+		JButton btnAvbryt = new JButton("Avbryt");
+		GridBagConstraints gbc_btnAvbryt = new GridBagConstraints();
+		btnAvbryt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		gbc_btnAvbryt.insets = new Insets(10, 10, 10, 10);
+		gbc_btnAvbryt.gridx = 2;
+		gbc_btnAvbryt.gridy = 7;
+		contentPane.add(btnAvbryt, gbc_btnAvbryt);
+	}
+
+}
