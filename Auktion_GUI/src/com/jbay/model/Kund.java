fc4456be83369673e47e438b99584a489f0a64d3
JoakimRezek <joakim.rezek@gmail.com>
2015-02-11 11:45:25 +0100

    kund inlagd

--- Auktion_GUI/src/com/jbay/model/Kund.java
+++ Auktion_GUI/src/com/jbay/model/Kund.java
@@ -1,5 +1,80 @@
 package com.jbay.model;
 
 public class Kund {
+	
+	private String personNummer;
+	private String forNamn;
+	private String efterNamn;
+	private String adress;
+	private String postNummer;
+	private String ort;
+	private String telefon;
+	private String email;
+	
+	
+	public Kund(String personNummer, String forNamn, String efterNamn,
+			String adress, String postNummer, String ort, String telefon,
+			String email) {
+		super();
+		this.personNummer = personNummer;
+		this.forNamn = forNamn;
+		this.efterNamn = efterNamn;
+		this.adress = adress;
+		this.postNummer = postNummer;
+		this.ort = ort;
+		this.telefon = telefon;
+		this.email = email;
+	}
+	
+	
+	
+	public String getPersonNummer() {
+		return personNummer;
+	}
+	public void setPersonNummer(String personNummer) {
+		this.personNummer = personNummer;
+	}
+	public String getForNamn() {
+		return forNamn;
+	}
+	public void setForNamn(String forNamn) {
+		this.forNamn = forNamn;
+	}
+	public String getEfterNamn() {
+		return efterNamn;
+	}
+	public void setEfterNamn(String efterNamn) {
+		this.efterNamn = efterNamn;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getPostNummer() {
+		return postNummer;
+	}
+	public void setPostNummer(String postNummer) {
+		this.postNummer = postNummer;
+	}
+	public String getOrt() {
+		return ort;
+	}
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+	public String getTelefon() {
+		return telefon;
+	}
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
